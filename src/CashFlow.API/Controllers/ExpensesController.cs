52ef079ad2b9a3124e5211812b97485dbc6a742c
matheushfp <mathzhf@gmail.com>
2025-01-20 17:09:16 -0300

    initial commit

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RegisterExpenseRequest request)
+    {
+        var useCase = new RegisterExpenseUseCase();
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public RegisterExpenseResponse Execute(RegisterExpenseRequest request)
+    {
+        // TODO => Validations
+
+        return new RegisterExpenseResponse();
+    }
+}
