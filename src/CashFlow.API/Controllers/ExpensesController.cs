2c824d3e65b149afd826a0c78e49ae3857b62a47
matheushfp <mathzhf@gmail.com>
2025-01-22 03:13:01 -0300

    add exception filter

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -14,24 +12,9 @@ public class ExpensesController : ControllerBase
     [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult Register([FromBody] RegisterExpenseRequest request)
     {
-        try
-        {
-            var useCase = new RegisterExpenseUseCase();
-            var response = useCase.Execute(request);
+        var useCase = new RegisterExpenseUseCase();
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch (ValidationErrorException ex)
-        {
-            var errorsResponse = new ErrorResponse(ex.Errors);
-
-            return BadRequest(errorsResponse);
-        }
-        catch
-        {
-            var errorsResponse = new ErrorResponse("Unknown Error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorsResponse);
-        }
+        return Created(string.Empty, response);
     }
 }
---
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,49 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnknownError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ValidationErrorException)
+        {
+            var ex = (ValidationErrorException)context.Exception;
+
+            var errorResponse = new ErrorResponse(ex.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+        else
+        {
+            var errorResponse = new ErrorResponse(context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+
+    private void ThrowUnknownError(ExceptionContext context)
+    {
+        var errorResponse = new ErrorResponse("Unknown error");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
