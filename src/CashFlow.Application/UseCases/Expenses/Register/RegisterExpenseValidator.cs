7af7e7174e4c384777b1b29d857e8d7d0bb0d29e
matheushfp <mathzhf@gmail.com>
2025-01-23 11:41:30 -0300

    feat: add resource error messages (en, pt-BR, fr)

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -1,5 +1,5 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -8,9 +8,9 @@ public class RegisterExpenseValidator : AbstractValidator<RegisterExpenseRequest
 {
     public RegisterExpenseValidator()
     {
-        RuleFor(expense => expense.Title).NotEmpty().WithMessage("Title is required");
-        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("Amount should be greater than 0");
-        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date of an expense cannot be in the future");
-        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Payment Type is not valid");
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_0);
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSE_CANNOT_BE_IN_THE_FUTURE);
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
     }
 }
