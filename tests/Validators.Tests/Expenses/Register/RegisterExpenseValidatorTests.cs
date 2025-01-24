286ccf40b262ca6c350f5fc799729c94f9ecdfe6
matheushfp <mathzhf@gmail.com>
2025-01-24 14:49:38 -0300

    test: add first success test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RegisterExpenseRequestBuilder.Build();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+}
