e5a07b7d394701bfe5227ea4ed4fbf9e3c980c2e
matheushfp <mathzhf@gmail.com>
2025-01-31 18:32:15 -0300

    feat: add unit of work

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionsBase;
 
@@ -9,10 +10,12 @@ namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;   
 
-    public RegisterExpenseUseCase(IExpensesRepository repository)
+    public RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public RegisterExpenseResponse Execute(RegisterExpenseRequest request)
@@ -29,6 +32,7 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
         };
 
         _repository.Add(entity);
+        _unitOfWork.Commit();
 
         return new RegisterExpenseResponse();
     }
--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -15,6 +15,5 @@ internal class ExpensesRepository : IExpensesRepository
     public void Add(Expense expense)
     {
         _dbContext.Expenses.Add(expense);
-        _dbContext.SaveChanges();
     }
 }
