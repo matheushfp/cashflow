3f3629554ea52ba98cd2fec79548e1b3dea82d49
matheushfp <mathzhf@gmail.com>
2025-02-10 19:28:39 -0300

    feat: add deleteExpense

--- src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
@@ -5,4 +5,10 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpensesWriteOnlyRepository
 {
     Task Add(Expense expense);
+    /// <summary>
+    /// This function returns true if deletion was successfull otherwise returns false
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> Delete(Guid id);
 }
--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -18,6 +18,18 @@ internal class ExpensesRepository : IExpensesReadOnlyRepository, IExpensesWriteO
         await _dbContext.Expenses.AddAsync(expense);
     }
 
+    public async Task<bool> Delete(Guid id)
+    {
+        var result = await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
+
+        if (result is null)
+            return false;
+
+        _dbContext.Expenses.Remove(result);
+
+        return true;
+    }
+
     public async Task<List<Expense>> GetAll()
     {
         return await _dbContext.Expenses.AsNoTracking().ToListAsync();
