6b95e40f074602006d4eb6337a67b175949d36d1
matheushfp <mathzhf@gmail.com>
2025-04-05 12:14:04 -0300

    test: add support for testing different cultures

--- tests/WebAPI.Tests/Users/Register/RegisterUserTest.cs
+++ tests/WebAPI.Tests/Users/Register/RegisterUserTest.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
-using CashFlow.Communication.Responses;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
+using WebAPI.Tests.InlineData;
 
 namespace WebAPI.Tests.Users.Register;
 public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
@@ -33,11 +35,14 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
         response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty().And.StartWith("eyJ");
     }
 
-    [Fact]
-    public async Task EmptyNameError()
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task EmptyNameError(string cultureInfo)
     {
         var request = RegisterUserRequestBuilder.Build();
         request.Name = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
 
         var result = await _httpClient.PostAsJsonAsync(REGISTER_USER_URI, request);
 
@@ -48,6 +53,8 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         var errors = response.RootElement.GetProperty("errors").EnumerateArray();
 
-        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.EMPTY_NAME));
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("EMPTY_NAME", new CultureInfo(cultureInfo));
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
     }
 }
