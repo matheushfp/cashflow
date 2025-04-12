ddf1852ca70d40f5eaeeef3d5ebdf179a3bdb65c
matheushfp <mathzhf@gmail.com>
2025-04-12 15:12:43 -0300

    test: add user login tests

--- tests/WebAPI.Tests/CustomWebApplicationFactory.cs
+++ tests/WebAPI.Tests/CustomWebApplicationFactory.cs
@@ -1,4 +1,7 @@
-﻿using CashFlow.Infrastructure.DataAccess;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Infrastructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +10,9 @@ using Microsoft.Extensions.DependencyInjection;
 namespace WebAPI.Tests;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -19,6 +25,27 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                     options.UseInternalServiceProvider(provider);
                 });
+
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+                var passwordEncrypter = scope.ServiceProvider.GetRequiredService<IPasswordEncrypter>();
+
+                StartDatabase(dbContext, passwordEncrypter);
             });
+    }
+
+    public string GetName() => _user.Name;
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password; 
+
+    private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncrypter passwordEncrypter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+
+        _user.Password = passwordEncrypter.Encrypt(_user.Password);
+
+        dbContext.Users.Add(_user);
+        dbContext.SaveChanges();
     }
 }
---
+++ tests/WebAPI.Tests/Users/Login/UserLoginTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using WebAPI.Tests.InlineData;
+
+namespace WebAPI.Tests.Users.Login;
+public class UserLoginTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string LOGIN_URI = "api/login";
+    private readonly HttpClient _httpClient;
+    private readonly string _email;
+    private readonly string _password;
+    private readonly string _name;
+
+    public UserLoginTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+        _email = webApplicationFactory.GetEmail();
+        _password = webApplicationFactory.GetPassword();
+        _name = webApplicationFactory.GetName();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new LoginRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        var result = await _httpClient.PostAsJsonAsync(LOGIN_URI, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseBody = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(responseBody);
+
+        response.RootElement.GetProperty("name").GetString().Should().Be(_name);
+        response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace().And.StartWith("eyJ");
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task InvalidLoginError(string cultureInfo)
+    {
+        var request = LoginRequestBuilder.Build();
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+
+        var result = await _httpClient.PostAsJsonAsync(LOGIN_URI, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+        var responseBody = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = response.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("INVALID_CREDENTIALS", new CultureInfo(cultureInfo));
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}
