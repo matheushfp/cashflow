f87ed349fd65691f70dd695759f35e28bc597a09
matheushfp <mathzhf@gmail.com>
2025-04-16 19:33:34 -0300

    feat: add logged user + get token from request

---
+++ src/CashFlow.API/Tokens/HttpContextTokenValue.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Security.Tokens;
+
+namespace CashFlow.API.Tokens;
+
+public class HttpContextTokenValue : ITokenProvider
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public HttpContextTokenValue(IHttpContextAccessor httpContextAccessor)
+    {
+        _contextAccessor = httpContextAccessor;
+    }
+
+
+    public string GetToken()
+    {
+        var authorization = _contextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+        return authorization["Bearer ".Length..].Trim();
+    }
+}
