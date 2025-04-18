a7cd87e7fa1f5afad872dfba21d653f8357b3e57
matheushfp <mathzhf@gmail.com>
2025-04-18 13:08:53 -0300

    feat: add logged user in create/update expense

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -4,6 +4,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -13,24 +14,33 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
     private readonly IExpensesWriteOnlyRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ILoggedUser _loggedUser;
 
-    public RegisterExpenseUseCase(IExpensesWriteOnlyRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+    public RegisterExpenseUseCase(
+        IExpensesWriteOnlyRepository repository,
+        IUnitOfWork unitOfWork,
+        IMapper mapper,
+        ILoggedUser loggedUser)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _loggedUser = loggedUser;
     }
 
     public async Task<RegisterExpenseResponse> Execute(ExpenseRequest request)
     {
         Validate(request);
 
-        var entity = _mapper.Map<Expense>(request);
+        var loggedUser = await _loggedUser.Get();
 
-        await _repository.Add(entity);
+        var expense = _mapper.Map<Expense>(request);
+        expense.UserId = loggedUser.Id;
+
+        await _repository.Add(expense);
         await _unitOfWork.Commit();
 
-        return _mapper.Map<RegisterExpenseResponse>(entity);
+        return _mapper.Map<RegisterExpenseResponse>(expense);
     }
 
     private void Validate(ExpenseRequest request)
