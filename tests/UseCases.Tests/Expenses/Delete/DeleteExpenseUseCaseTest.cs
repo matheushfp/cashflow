e0fb09427beab15b30d5e9b8787ba67ef9ef0712
matheushfp <mathzhf@gmail.com>
2025-05-24 14:10:51 -0300

    test: add delete expense tests

---
+++ tests/UseCases.Tests/Expenses/Delete/DeleteExpenseUseCaseTest.cs
@@ -0,0 +1,49 @@
+﻿using CashFlow.Application.UseCases.Expenses.Delete;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Tests.Expenses.Delete;
+public class DeleteExpenseUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build();
+        var expense = ExpenseBuilder.Build(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expense);
+
+        var act = async () => await useCase.Execute(expense.Id);
+
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async Task ExpenseNotFoundError()
+    {
+        var loggedUser = UserBuilder.Build();
+
+        var useCase = CreateUseCase(loggedUser);
+
+        var act = async () => await useCase.Execute(id: Guid.NewGuid());
+
+        var result = await act.Should().ThrowAsync<NotFoundException>();
+
+        result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.EXPENSE_NOT_FOUND));
+    }
+
+    private DeleteExpenseUseCase CreateUseCase(User user, Expense? expense = null)
+    {
+        var readOnlyRepository = new ExpensesReadOnlyRepositoryBuilder().GetById(user, expense).Build();
+        var writeOnlyRepository = ExpensesWriteOnlyRepositoryBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new DeleteExpenseUseCase(readOnlyRepository, writeOnlyRepository, unitOfWork, loggedUser);
+    }
+}
---
+++ tests/WebAPI.Tests/Expenses/Delete/DeleteExpenseTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using WebAPI.Tests.InlineData;
+
+namespace WebAPI.Tests.Expenses.Delete;
+public class DeleteExpenseTest : CashFlowClassFixture
+{
+    private const string DELETE_EXPENSE_URI = "api/expenses";
+
+    private readonly string _token;
+    private readonly Guid _expenseId;
+
+    public DeleteExpenseTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.UserTeamMember.GetToken();
+        _expenseId = webApplicationFactory.Expense.GetId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoDelete(requestUri: $"{DELETE_EXPENSE_URI}/{_expenseId}", token: _token);
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+
+        result = await DoGet(requestUri: $"{DELETE_EXPENSE_URI}/{_expenseId}", token: _token);
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task ExpenseNotFoundError(string culture)
+    {
+        var result = await DoDelete(requestUri: $"{DELETE_EXPENSE_URI}/{Guid.NewGuid()}", token: _token, culture: culture);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var responseBody = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = response.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("EXPENSE_NOT_FOUND", new CultureInfo(culture));
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+}
